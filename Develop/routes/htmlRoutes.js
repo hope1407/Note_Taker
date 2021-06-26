3f49656e2cff2e790fe7df9acfcba00065202f6d
Hope <johnhope354@gmail.com>
2021-06-25 22:57:28 -0500

    functioning code

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+// ROUTING
+
+module.exports = (app) => {
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
