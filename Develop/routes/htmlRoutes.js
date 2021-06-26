692b8aa31a44ae66073c62b2217a81b707a2136f
Hope <johnhope354@gmail.com>
2021-06-26 14:33:12 -0500

    added app.get('/'...)

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,6 +3,9 @@ const path = require('path');
 // ROUTING
 
 module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
